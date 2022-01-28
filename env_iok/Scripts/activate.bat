ea8867ec002682381a69b3746865905d8e4e3fc9
eliaskontos <62223521+eliaskontos@users.noreply.github.com>
2022-01-28 20:52:21 +0200

    dsa

--- env_iok/Scripts/activate.bat
+++ env_iok/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\elias\source\repos\Iok Ergasia\env_iok
+set VIRTUAL_ENV=C:\Users\elias\Source\Repos\Ergasia_Iok\env_iok
 
 if not defined PROMPT set PROMPT=$P$G
 
