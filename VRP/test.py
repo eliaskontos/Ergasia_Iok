9c0878dda4b64f9ad35cd777a52f2971784f0386
eliaskontos <62223521+eliaskontos@users.noreply.github.com>
2023-05-07 14:47:21 +0300

    htyh

---
+++ VRP/test.py
@@ -0,0 +1,27 @@
+import random
+
+class Node:
+    def __init__(self, id, dem, xx, yy):
+        self.ID = id
+        self.demand = dem
+        self.x = xx
+        self.y = yy
+
+# seed
+random.seed(1)
+all_nodes = []
+customers = []
+depot = Node(0, 0, 50, 50)
+all_nodes.append(depot)
+
+for i in range(0, 100):
+    id= i + 1
+    dem= random.randint(1, 6)
+    xx = random.randint(0, 100)
+    yy = random.randint(0, 100)
+    cust = Nodes(id, dem, xx, yy)
+    all_nodes.append(cust)
+    customers.append(cust)
+
+
+    
